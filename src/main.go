371c7441f5c0f9de8809c4b8f2cb6ca3196ca390
mononomo <hostdog01@naver.com>
2022-06-06 23:54:25 +0900

    feat(html) : simple html server

--- src/main.go
+++ src/main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
+type problem struct {
+	content string
+	id      string
+}
+
+func basicRouter(w http.ResponseWriter, r *http.Request) {
+	var localPath string = "public/"
+	content, err := ioutil.ReadFile(localPath + r.URL.Path[1:])
+	if err != nil {
+		w.WriteHeader(404)
+		w.Write([]byte(http.StatusText(404)))
+		return
+	}
+	w.Write(content)
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-	})
-	if error := http.ListenAndServe(":8080", nil); error != nil {
+	http.HandleFunc("/", basicRouter)
+	if error := http.ListenAndServe(":3000", nil); error != nil {
 		panic(error)
 	}
 }
+
+/*
+	nil 은 포인터, 인터페이스, 맵, 슬라이스, 채널, 함수의 zero value 입니다.
+
+
+*/
